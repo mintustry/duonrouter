b0d3ccee0b3bd02e46e3ce9e8261ccdce1498504
mintustry <70564305+mintustry@users.noreply.github.com>
2021-05-24 17:53:43 +1000

    rotate it
    yes

---
+++ scripts/blocks/dingus-rotate-thing.js
@@ -0,0 +1,19 @@
+const rotateBlock = extend(DrawMixer, {
+    draw(entity){
+        const rotation = entity.block.rotate ? entity.rotdeg() : 0;
+
+        Draw.rect(this.bottom, entity.x, entity.y, rotation);
+        Draw.rect(this.rotator, entity.x, entity.y, entity.totalProgress * 6);
+    },
+    load(block){
+        this.rotator = Core.atlas.find(block.name + "-rotator");
+    },
+    icons(block){
+        return [
+          this.rotator
+        ];
+    }
+});
+// stole it from Sharlottes/ExampleMod
+const atmosphericCooler = extendContent(GenericCrafter, "dingus-rotate-thing", {});
+atmosphericCooler.drawer = rotateBlock;
