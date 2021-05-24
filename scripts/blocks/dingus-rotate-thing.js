bf144bf21b8bbf9955fef6a382a27b12cee5a8e2
mintustry <70564305+mintustry@users.noreply.github.com>
2021-05-24 18:05:51 +1000

    Update dingus-rotate-thing.js

--- scripts/blocks/dingus-rotate-thing.js
+++ scripts/blocks/dingus-rotate-thing.js
@@ -2,17 +2,23 @@ const rotateBlock = extend(DrawMixer, {
     draw(entity){
         const rotation = entity.block.rotate ? entity.rotdeg() : 0;
 
+        Draw.rect(this.bottom, entity.x, entity.y, rotation);
         Draw.rect(this.rotator, entity.x, entity.y, entity.totalProgress * 6);
+        Draw.rect(this.top, entity.x, entity.y, rotation);
     },
     load(block){
         this.rotator = Core.atlas.find(block.name + "-rotator");
+        this.top = Core.atlas.find(block.name + "-top");
+        this.bottom = Core.atlas.find(block.name + "-bottom");
     },
     icons(block){
         return [
-          this.rotator
+          this.bottom,
+          this.rotator,
+          this.top
         ];
     }
 });
-// stole it from Sharlottes/ExampleMod
-const atmosphericCooler = extendContent(GenericCrafter, "dingus-rotate-thing", {});
+//stole it form Sharlottes/ExampleMod
+const atmosphericCooler = extendContent(GenericCrafter, "atmospheric-cooler", {});
 atmosphericCooler.drawer = rotateBlock;
