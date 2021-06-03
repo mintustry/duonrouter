336400327dc65df935906ba936293efa17ba1aba
mintustry <70564305+mintustry@users.noreply.github.com>
2021-06-03 23:24:49 +1000

    Update zap.js

--- scripts/units/zap.js
+++ scripts/units/zap.js
@@ -1,9 +1,10 @@
 const ais = require("libs/ai");
 
-var minSpd = 0;
-var maxSpd = 100;
-var col = Color.valueOf("#a9d8ff");
+let minSpd = 1;
+let maxSpd = 100;
+let col = Color.valueOf("#a9d8ff");
 const aaa = extend(UnitType, "zap", {});
 
-aaa.defaultController = ais.flareAI(95, [BlockFlag.factory, BlockFlag.battery, BlockFlag.generator, BlockFlag.core]);
-aaa.abilities.add(new MoveLightningAbility(6.4 * Vars.state.rules.unitDamageMultiplier, 11, 0.3, 19/4, minSpd, maxSpd, Color.valueOf("#a9d8ff"),"1"));
+aaa.constructor = () => extend(UnitEntity, {});
+aaa.defaultController = ais.noWeaponFlareAI(70, [BlockFlag.factory, BlockFlag.battery, BlockFlag.generator, BlockFlag.core]);
+aaa.abilities.add(new MoveLightningAbility(6 * Vars.state.rules.unitDamageMultiplier, 8, 0.3, 17/4, minSpd, maxSpd, col, "purple-air-needle-heat"));
