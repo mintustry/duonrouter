7ae0e9acb36764539208607563efdcfba9c85327
mintustry <70564305+mintustry@users.noreply.github.com>
2021-06-03 23:16:26 +1000

    Update zap.js

--- scripts/units/zap.js
+++ scripts/units/zap.js
@@ -6,4 +6,4 @@ const zaps = extendContent(UnitType, "zap", {
 });
 
 zaps.defaultController = ais.flareAI(95, [BlockFlag.factory, BlockFlag.battery, BlockFlag.generator, BlockFlag.core]);
-zaps.abilities.add(new MoveLightningAbility(6.4 * Vars.state.rules.unitDamageMultiplier, 11, 0.3, 19/4, minSpd, maxSpd, Color.valueOf("#a9d8ff")));
+zaps.abilities.add(new MoveLightningAbility(6.4 * Vars.state.rules.unitDamageMultiplier, 11, 0.3, 19/4, minSpd, maxSpd, Color.valueOf("#a9d8ff"),"1"));
